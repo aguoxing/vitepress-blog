748ae91204b880a4d5f4babfb7c03e61f4b01e7d
gwoksing98 <gwoksing98@qq.com>
2022-07-25 12:25:17 +0800

    build doc

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,24 @@ yarn docs:build
 
 # 生成静态文件后先推到master分支
 git add -A
-git commit -m 'update'
+git commit -m 'build doc'
 git push origin
 
 # 进入生成的文件夹
 cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
-# echo 'xx.com' > CNAME
+echo 'www.cloudos.top' > CNAME
 
 git init
 git add -A
-git commit -m 'update'
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@gitee.com:<USERNAME>/<USERNAME>.gitee.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push <远程主机名> <本地分支名>:<远程分支名>
 git push -f git@github.com:aguoxing/vitepress-blog.git master:gh-pages
 
 cd -
