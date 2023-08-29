bec16caccb4302a99a99961053021f07ede45e68
aguoxing <2933575030@qq.com>
2023-08-29 16:23:33 +0800

    chore: update deps

--- docs/.vitepress/cache/deps/vitepress___@vueuse_integrations_useFocusTrap.js
+++ docs/.vitepress/cache/deps/vitepress___@vueuse_integrations_useFocusTrap.js
@@ -18,7 +18,7 @@ function tryOnScopeDispose(fn) {
 function toValue(r) {
   return typeof r === "function" ? r() : unref(r);
 }
-var isClient = typeof window !== "undefined";
+var isClient = typeof window !== "undefined" && typeof document !== "undefined";
 var isIOS = getIsIOS();
 function getIsIOS() {
   var _a;
@@ -1114,40 +1114,9 @@ var createFocusTrap = function createFocusTrap2(elements, userOptions) {
 };
 
 // node_modules/@vueuse/integrations/useFocusTrap.mjs
-var __defProp = Object.defineProperty;
-var __defProps = Object.defineProperties;
-var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
-var __getOwnPropSymbols = Object.getOwnPropertySymbols;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __propIsEnum = Object.prototype.propertyIsEnumerable;
-var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-var __spreadValues = (a, b) => {
-  for (var prop in b || (b = {}))
-    if (__hasOwnProp.call(b, prop))
-      __defNormalProp(a, prop, b[prop]);
-  if (__getOwnPropSymbols)
-    for (var prop of __getOwnPropSymbols(b)) {
-      if (__propIsEnum.call(b, prop))
-        __defNormalProp(a, prop, b[prop]);
-    }
-  return a;
-};
-var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
-var __objRest = (source, exclude) => {
-  var target = {};
-  for (var prop in source)
-    if (__hasOwnProp.call(source, prop) && exclude.indexOf(prop) < 0)
-      target[prop] = source[prop];
-  if (source != null && __getOwnPropSymbols)
-    for (var prop of __getOwnPropSymbols(source)) {
-      if (exclude.indexOf(prop) < 0 && __propIsEnum.call(source, prop))
-        target[prop] = source[prop];
-    }
-  return target;
-};
 function useFocusTrap(target, options = {}) {
   let trap;
-  const _a = options, { immediate } = _a, focusTrapOptions = __objRest(_a, ["immediate"]);
+  const { immediate, ...focusTrapOptions } = options;
   const hasFocus = ref(false);
   const isPaused = ref(false);
   const activate = (opts) => trap && trap.activate(opts);
@@ -1169,7 +1138,8 @@ function useFocusTrap(target, options = {}) {
     (el) => {
       if (!el)
         return;
-      trap = createFocusTrap(el, __spreadProps(__spreadValues({}, focusTrapOptions), {
+      trap = createFocusTrap(el, {
+        ...focusTrapOptions,
         onActivate() {
           hasFocus.value = true;
           if (options.onActivate)
@@ -1180,7 +1150,7 @@ function useFocusTrap(target, options = {}) {
           if (options.onDeactivate)
             options.onDeactivate();
         }
-      }));
+      });
       if (immediate)
         activate();
     },
