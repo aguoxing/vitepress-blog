41086c22c1aace3270d07b1952c5a07768487e61
aguoxing <2933575030@qq.com>
2025-07-30 15:02:49 +0800

    build doc

---
+++ docs/.vitepress/dist/assets/posts_blender.md.CeSXopUx.js
@@ -0,0 +1,13 @@
+import{_ as n,c as a,o as e,ag as p}from"./chunks/framework.S-Qvb3wi.js";const m=JSON.parse('{"title":"Blender快捷键记录","description":"Blender快捷键记录","frontmatter":{"title":"Blender快捷键记录","description":"Blender快捷键记录","date":"2023-12-13T00:00:00.000Z","category":"建模","tags":["Blender"]},"headers":[],"relativePath":"posts/blender.md","filePath":"posts/blender.md","lastUpdated":1753857155000}'),l={name:"posts/blender.md"};function r(i,s,t,c,b,d){return e(),a("div",null,s[0]||(s[0]=[p(`<div class="language- vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>g 移动 z 底部对齐场景</span></span>
+<span class="line"><span>tab 编辑模式</span></span>
+<span class="line"><span>数字 2 边模式</span></span>
+<span class="line"><span>数字 3 面模式 p 独立出来</span></span>
+<span class="line"><span>ctrl b 倒角</span></span>
+<span class="line"><span>ctrl r 环切</span></span>
+<span class="line"><span>i 内插面 e 向内</span></span>
+<span class="line"><span>shift d 复制</span></span>
+<span class="line"><span>/ 独立显示模式</span></span>
+<span class="line"><span>alt w 切割模式 x</span></span>
+<span class="line"><span>alt x 镜像</span></span>
+<span class="line"><span>j 合并</span></span>
+<span class="line"><span>shift s 回到游标原点</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br></div></div><p>参考 <a href="https://www.bilibili.com/video/BV1884y1k7mB/" target="_blank" rel="noreferrer">https://www.bilibili.com/video/BV1884y1k7mB/</a>?</p>`,2)]))}const u=n(l,[["render",r]]);export{m as __pageData,u as default};
